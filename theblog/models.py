50867cb0ba69b8889c73a0e18dc0ea9831c1164b
tetsuya <tetsuyaf1100@gmail.com>
2021-12-28 15:10:40 +0900

    Django Blog #4 Add Blog Post Page

--- theblog/models.py
+++ theblog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -9,3 +10,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
+
+    def get_absolute_url(self):
+        #return reverse('article-detail', args=(str(self.id)))
+        return reverse('home')
