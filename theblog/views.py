732a7b31e0fc3faede4fa025145f9d17d907ea42
tetsuya <tetsuyaf1100@gmail.com>
2022-01-01 17:05:02 +0900

    Django Blog #15 How To Add Blog Category Navbar Links

--- theblog/views.py
+++ theblog/views.py
@@ -8,13 +8,20 @@ from django.urls import reverse_lazy
 #    return render(request, 'home.html', {})
 def CategoryView(request, cats):
     category_posts = Post.objects.filter(category=cats)
-    return render(request, 'categories.html', {'cats':cats, 'category_posts':category_posts})
+    cat_menu = Category.objects.all()
+    return render(request, 'categories.html', {'cats':cats, 'category_posts':category_posts, 'cat_menu':cat_menu})
 
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
     ordering = ['-post_date']
     #ordering = ['-id']
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        print(context["cat_menu"])
+        return context
 
 class ArticleDetailView(DetailView):
     model = Post
