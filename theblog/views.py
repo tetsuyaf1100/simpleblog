a9aa66b2af414721799f72eff01b21e4ab35efe8
tetsuya <tetsuyaf1100@gmail.com>
2022-01-06 21:06:33 +0900

    Django Blog #18 Create Blog Like Button

--- theblog/views.py
+++ theblog/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post, Category
 from .forms import PostForm, EditForm
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.http import HttpResponseRedirect
+
+def LikeView(request, pk):
+    post = get_object_or_404(Post, id=request.POST.get('post_id'))
+    post.likes.add(request.user)
+    return HttpResponseRedirect(reverse('article-detail', args=[str(pk)])) 
 
 #def home(request):
 #    return render(request, 'home.html', {})
@@ -26,6 +32,16 @@ class HomeView(ListView):
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
+
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(ArticleDetailView, self).get_context_data(*args, **kwargs)
+        
+        stuff = get_object_or_404(Post, id=self.kwargs['pk'])
+        total_likes = stuff.total_likes()
+        context["cat_menu"] = cat_menu
+        context["total_likes"] = total_likes
+        return context
 
 class AddPostView(CreateView):
     model = Post
